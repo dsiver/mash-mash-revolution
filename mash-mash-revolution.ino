4f6b2804f67e2af507b0744d5aec76c5e5f2864a
David Siver <dsiver1@my.westga.edu>
2015-10-19 23:29:35 -0400

    can draw all four circles

--- mash-mash-revolution.ino
+++ mash-mash-revolution.ino
@@ -19,6 +19,12 @@ void setup() {
   EsploraTFT.begin();
   EsploraTFT.background(0, 0, 0);
   drawMashFloor();
+  
+  
+  drawCircleOne();
+  drawCircleTwo();
+  drawCircleThree();
+  drawCircleFour();
 }
 
 void loop() {
@@ -27,13 +33,38 @@ void loop() {
 }
 
 void drawMashFloor() {
-  EsploraTFT.stroke(255, 255, 255);
   //EsploraTFT.line(xStart, yStart, xEnd, yEnd);
+  EsploraTFT.stroke(255, 255, 255);
   EsploraTFT.line(40, 0, 0, 40);
   EsploraTFT.line(0, 40, 40, 80);
   EsploraTFT.line(40, 80, 80, 40);
   EsploraTFT.line(80, 40, 40, 0);
-
   EsploraTFT.line(20, 20, 60, 60);
   EsploraTFT.line(20, 60, 60, 20);
 }
+
+void drawCircleOne(){
+  //EsploraTFT.circle(xPos, yPos, radius);
+  EsploraTFT.stroke(0,255,0);
+  EsploraTFT.fill(0,255,0);
+  EsploraTFT.circle(40, 60, 10);
+}
+
+void drawCircleTwo(){
+    EsploraTFT.stroke(255,0,0);
+  EsploraTFT.fill(255,0,0);
+  EsploraTFT.circle(20, 40, 10);
+}
+
+void drawCircleThree(){
+  EsploraTFT.stroke(0,255,255);
+  EsploraTFT.fill(0, 255,255);
+  EsploraTFT.circle(40, 20, 10);
+}
+
+void drawCircleFour(){
+  EsploraTFT.stroke(0,0,255);
+  EsploraTFT.fill(0,0,255);
+  EsploraTFT.circle(60, 40, 10);
+}
+
