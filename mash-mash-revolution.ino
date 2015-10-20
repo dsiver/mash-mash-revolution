64f2710c16ce27afabfe9f7d23b88c7045bd9df3
David Siver <dsiver1@my.westga.edu>
2015-10-19 23:15:59 -0400

    drawing grid for circles

--- mash-mash-revolution.ino
+++ mash-mash-revolution.ino
@@ -7,20 +7,33 @@
  * Fall 2015
  */
 
-#include <Esplora.h>
 #include <SPI.h>
 #include <TFT.h>
+#include <Esplora.h>
 
 #define DEBUG 1
 #define SERIAL_BAUD_RATE 9600
 
 void setup() {
-  Serial.begin(serialBaudRate);
+  Serial.begin(SERIAL_BAUD_RATE);
   EsploraTFT.begin();
   EsploraTFT.background(0, 0, 0);
+  drawMashFloor();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void drawMashFloor() {
+  EsploraTFT.stroke(255, 255, 255);
+  //EsploraTFT.line(xStart, yStart, xEnd, yEnd);
+  EsploraTFT.line(40, 0, 0, 40);
+  EsploraTFT.line(0, 40, 40, 80);
+  EsploraTFT.line(40, 80, 80, 40);
+  EsploraTFT.line(80, 40, 40, 0);
+
+  EsploraTFT.line(20, 20, 60, 60);
+  EsploraTFT.line(20, 60, 60, 20);
 }
